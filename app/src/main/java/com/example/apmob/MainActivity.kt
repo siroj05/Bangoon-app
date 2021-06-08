df0a921157590c4b9145ae714920833b5cce8e33
auzora <frdluthfy@gmail.com>
2021-06-08 10:50:47 +0700

    added firebase authentication

--- app/src/main/java/com/example/apmob/MainActivity.kt
+++ app/src/main/java/com/example/apmob/MainActivity.kt
@@ -2,3 +2,12 @@ package com.example.apmob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+class MainActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceStates: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.Login)
+        btnAddTodo.setOnClickListener(this);
+    }
+
+}
