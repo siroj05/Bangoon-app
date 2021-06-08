3a4839dbb30b9d6ef2bd27ba2dcc7276f40e9ca1
Sirojjuddin Apendi <rojudin123@gmail.com>
2021-06-08 12:40:02 +0700

    ya tambah2in aja

--- app/src/main/java/com/example/apmob/MainActivity.kt
+++ app/src/main/java/com/example/apmob/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.apmob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.main_menu.*
 
 class MainActivity:AppCompatActivity(){
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceStates: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.Login)
-        btnAddTodo.setOnClickListener(this);
+        imageSlide.setOnClickListener(this);
     }
 
 }
